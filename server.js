0183162b741e610221d67aad66277936b6e7c725
Benoît Gelineau <39692884+benoitgelineau@users.noreply.github.com>
2018-07-10 10:49:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -14,9 +14,15 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json(
+    {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    }
+  );
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
